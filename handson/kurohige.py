34a22847cbeed96dbe5689da9088cebefda364df
yattinda <yattinda@gmail.com>
2020-02-26 16:36:21 +0900

    fuck

---
+++ handson/kurohige.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+import os
+import random
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+#環境変数取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self,username):
+        self.username = username
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    user = User(event.message.text)
+    db.session.add(user)
+    db.session.commit()
+    contents = db.session.query(User).all()
+
+
+    for content in contents:
+        messages.append(TextSendMessage(content.username))
+
+    messages =[]
+
+
+
+
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        messages[-5:]
+    TextSendMessage(text=finalreturn))
+
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
