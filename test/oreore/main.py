479af76f97ff882b9d0990b543b58b980c9f57ab
yattinda <yattinda@gmail.com>
2020-11-04 02:46:49 +0900

    Test pushtext & get user_id

--- test/oreore/main.py
+++ test/oreore/main.py
@@ -32,13 +32,14 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String, unique=False)
 
     def __init__(self, user_id):
         self.user_id = user_id
-
+"""
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -56,14 +57,14 @@ def callback():
 
     return 'OK'
 
+"""
 @handler.add(FollowEvent)
 def handle_join(event):
     user_id = line_bot_api.get_profile(event.source.user_id)
     db.session.add(user_id)
     db.session.commit()
-    contents = db.session.query(User).all()
     msg = []
-    msg.append(TextSendMessage(text=anan))
+    msg.append(TextSendMessage(text="anan"))
     line_bot_api.reply_message(event.reply_token,msg)
 
 
@@ -74,8 +75,15 @@ def handle_message(event):
     for i in contents:
         line_bot_api.push_message('i', TextSendMessage(text="AhiAhi"))
 
+"""
+@handler.add(MessageEvent, message=TextMessage)
+def main(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
 
+    print(profile.user_id)
 
+    messages = TextSendMessage(text=profile.user_id)
+    line_bot_api.push_message(profile.user_id, messages=messages)
 
 if __name__ == "__main__":
 #    app.run()
